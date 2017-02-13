1c48464bc9ef9a088be172b99b6597f6c6501bff
strivetobelazy <strivetobelazy@gmail.com>
2017-02-13 01:49:29 +0100

    Added aliases for git

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,10 +1,7 @@
+#vim alias
 alias vi='nvim -u NONE'
 alias e='nvim'
 alias vim='nvim'
-
-# alias gcc='gcc-6'
-# alias g++='g++-6'
-
 
 #tmux alias
 alias ta='tmux attach -t'
@@ -13,3 +10,9 @@ alias tls='tmux ls'
 alias tsend='tmux send -t'
 alias tkill='tmux kill-session -t'
 alias ts='tmuxinator start'
+
+#git alias
+alias gstatus='git status'
+alias gwrite='git add'
+alias gcommit='git commit'
+alias gdiff='git diff'
