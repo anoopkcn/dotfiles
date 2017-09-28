5ea4b2fbbb9a48e5ab61b550ae6ff39da8ee7b4b
anoop chandran <strivetobelazy@gmail.com>
2017-09-29 01:46:10 +0200

    updated installation script

--- install.sh
+++ install.sh
@@ -4,15 +4,10 @@
 # (Optional)install Ag using `brew install the_silver_searcher`
 # tmux is configured using reattach-usernamespace (install using brew)
 
-ln -sf ~/Dropbox/Dotfiles/bash/bash_profile ~/.bash_profile
-ln -sf ~/Dropbox/Dotfiles/bash/bashrc ~/.bashrc
-ln -sf ~/Dropbox/Dotfiles/bash/inputrc ~/.inputrc
-
-ln -sf ~/Dropbox/Dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -sf ~/Dropbox/Dotfiles/tmux/tmuxinator ~/.tmuxinator
-
-ln -sf ~/Dropbox/Dotfiles/python/pip ~/.pip
-ln -sf ~/Dropbox/Dotfiles/git/gitconfig ~/.gitconfig
-
-ln -sf ~/Dropbox/Dotfiles/vim/init.vim ~/.vimrc
-ln -sf ~/Dropbox/Dotfiles/vim ~/.vim
+ln -sf ~/Dropbox/dotfiles/bash/bashrc ~/.bashrc
+ln -sf ~/Dropbox/dotfiles/bash/bash_profile ~/.bash_profile
+ln -sf ~/Dropbox/dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/Dropbox/dotfiles/bash/lazy ~/.bash_it/themes/lazy
+ln -sf /Users/lazy/Dropbox/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf /Users/lazy/Dropbox/dotfiles/tmux/tmuxinator ~/.tmuxinator
+ln -sf /Users/lazy/Dropbox/dotfiles/vim ~/.config/nvim
