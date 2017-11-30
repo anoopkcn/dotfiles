3c364062c5a3259bb672884c0ad89465ac39a976
anoop chandran <strivetobelazy@gmail.com>
2017-11-30 19:33:51 +0100

    plugged removed

--- bash/functions.sh
+++ bash/functions.sh
@@ -96,51 +96,65 @@ function extract() {
     fi
 }
 
-doitobib(){
-     if [[ $# == 1 ]]; then
-      echo >> bib.bib;
-      curl -s "http://api.crossref.org/works/$1/transform/application/x-bibtex" >> bib.bib;
-      echo >> bib.bib;
-     else
-      echo -n "DOI = $1,  "
-      curl -s "http://api.crossref.org/works/$1/transform/application/x-bibtex" | grep $2 | sed "s/^[ \t]*//"
-     fi
-}
+# doitobib(){
+#      if [[ $# == 1 ]]; then
+#       echo >> bib.bib;
+#       curl -s "http://api.crossref.org/works/$1/transform/application/x-bibtex" >> bib.bib;
+#       echo >> bib.bib;
+#      else
+#       echo -n "DOI = $1,  "
+#       curl -s "http://api.crossref.org/works/$1/transform/application/x-bibtex" | grep $2 | sed "s/^[ \t]*//"
+#      fi
+# }
+
 doi2bib(){
-BIB_FILE=""
+BIB_FILE=""; BIB_INFO="";
+BIB_API="http://api.crossref.org/works/"
+BIB_TRANS="/transform/application/x-bibtex"
 while test $# -gt 0; do
-    case "$1" in
-        -h|--help)
-            echo "convert a doi to bibtex friendly file"
-            exit 0
-            ;;
-        -f|--file)
-            shift
-            if test $# -gt 0; then
-                    BIB_FILE=$1
-            fi
-            shift
-            ;;
-        -d|--doi)
-            shift
-            if test $# -gt 0; then
-                    DOI=$1
-            else
-                    echo "no DOI specified"
-                    exit 1
-            fi
-            shift
-            ;;
-        *)
-            break
-            ;;
-    esac
+  case "$1" in
+    -h|--help)
+      echo "Processing info using a DOI"
+      exit 0
+      ;;
+    -d|--doi)
+      shift
+      if test $# -gt 0; then
+        DOI=$1
+      else
+        echo "No DOI Specified"; exit 1
+      fi
+      shift
+      ;;
+    -i|--info)
+      shift
+      if test $# -gt 0; then
+        BIB_INFO=$1
+      fi
+      shift
+      ;;
+    -f|--file)
+      shift
+      if test $# -gt 0; then
+        BIB_FILE=$1
+      fi
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
 done
+# Use the information gatherd above
 if [[  -z  $BIB_FILE  ]];then
-    curl -s "http://api.crossref.org/works/${DOI}/transform/application/x-bibtex"
+  if [[ ! -z $BIB_INFO ]];then
+    curl -s "${BIB_API}${DOI}${BIB_TRANS}" | grep ${BIB_INFO} | sed "s/^[ \t]*//"
+  else
+    curl -s "${BIB_API}${DOI}${BIB_TRANS}"
+  fi
 elif [[  ! -z  $BIB_FILE  ]];then
     echo >> ${BIB_FILE};
-    curl -s "http://api.crossref.org/works/${DOI}/transform/application/x-bibtex" >> ${BIB_FILE}
+    curl -s "${BIB_API}${DOI}${BIB_TRANS}" >> ${BIB_FILE}
     echo >> ${BIB_FILE};
 fi
 }
