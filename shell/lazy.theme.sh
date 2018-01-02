d13f609bbf9bb7aab4b2307e338ddc7651da5326
anoop chandran <strivetobelazy@gmail.com>
2018-01-02 08:30:51 +0100

    new settings for zsh and shell in general

---
+++ shell/lazy.theme.sh
@@ -0,0 +1,34 @@
+#color definitions
+PS3=">> "
+PS2="▪ "
+# PS1="▪"'$(__git_ps1 "(%s)")'"[${cyan}\W${normal}]"
+# PS1="▪$(lazy_git_prompt)[${cyan}\W${normal}]"
+# export PS1="▪\$(lazy_git_status)[${cyan}\W${normal}]"
+#for oh-my-zsh
+# PROMPT="▪\$(lazy_git_status)[${cyan}\W${normal}]"
+local ret_status="%(?:%{$fg_bold[green]%}▪%{$reset_color%}:%{$fg_bold[red]%}▪%{$reset_color%})"
+PROMPT='${ret_status}$(lazy_git_status)[%{$fg[cyan]%}%c%{$reset_color%}]'
+
+lazy_git_status() {
+  # Get the current git branch name (if available)
+  local ref=$(git symbolic-ref HEAD 2>/dev/null | cut -d'/' -f3)
+  if [[ "$ref" != "" ]];then
+  (
+    IFS='\n';
+    git_status=$(git status -s 2>/dev/null | cut -c1-2)
+    if [[ -z "$git_status" ]]; then
+      echo "(%{$fg[blue]%}${ref}%{$reset_color%} %{$fg_bold[green]%}✓%{$reset_color%})"
+    else
+      git_staged=$(echo ${git_status} | grep -c "^[M|A|D|R|C]")
+      [[ $git_staged = 0 ]] && git_staged="" || git_staged=" ${git_staged}%{$fg_bold[yellow]%}ᴹ%{$reset_color%}"
+      git_modified=$(echo ${git_status} | grep -c "^[M|A|D|R|C|[:space:]][M|A|R|C]")
+      [[ $git_modified = 0 ]] && git_modified="" || git_modified=" ${git_modified}%{$fg_bold[green]%}ᴹ%{$reset_color%}"
+      git_deleted=$(echo ${git_status} | grep -c "^[[:space:]]D")
+      [[ $git_deleted = 0 ]] && git_deleted="" || git_deleted=" ${git_deleted}%{$fg_bold[red]%}ᴰ%{$reset_color%}"
+      git_untracked=$(echo ${git_status} | grep -c "??")
+      [[ $git_untracked = 0 ]] && git_untracked="" || git_untracked=" ${git_untracked}%{$fg_bold[cyan]%}ˀ%{$reset_color%}"
+      echo "(%{$fg[blue]%}${ref}%{$reset_color%}${git_staged}${git_modified}${git_deleted}${git_untracked})"
+    fi
+  )
+  fi
+}
