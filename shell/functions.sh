05e044b8315806396c5e24de9bb0fa9ab5fae8da
anoop chandran <strivetobelazy@gmail.com>
2018-11-30 10:17:57 +0100

    Changes to vimrc mainly

--- shell/functions.sh
+++ shell/functions.sh
@@ -10,23 +10,23 @@ case "${unameOut}" in
     *)          MACHINE="UNKNOWN:${unameOut}"
 esac
 #
-function os-template(){
-  if [[ "$OSTYPE" == "linux-gnu" ]]; then
-          # ...
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-          # Mac OSX
-  elif [[ "$OSTYPE" == "cygwin" ]]; then
-          # POSIX compatibility layer and Linux environment emulation for Windows
-  elif [[ "$OSTYPE" == "msys" ]]; then
-          # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-  elif [[ "$OSTYPE" == "win32" ]]; then
-          # I'm not sure this can happen.
-  elif [[ "$OSTYPE" == "freebsd"* ]]; then
-          # ...
-  else
+# function os-template(){
+#   if [[ "$OSTYPE" == "linux-gnu" ]]; then
+#           # ...
+#   elif [[ "$OSTYPE" == "darwin"* ]]; then
+#           # Mac OSX
+#   elif [[ "$OSTYPE" == "cygwin" ]]; then
+#           # POSIX compatibility layer and Linux environment emulation for Windows
+#   elif [[ "$OSTYPE" == "msys" ]]; then
+#           # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+#   elif [[ "$OSTYPE" == "win32" ]]; then
+#           # I'm not sure this can happen.
+  # elif [[ "$OSTYPE" == "freebsd"* ]]; then
+  #         # ...
+  # else
           # Unknown.
-  fi
-}
+  # fi
+# }
 
 function system(){
     echo "Hello, $USER"
