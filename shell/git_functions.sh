ce5bd3967e78eabc9623e229bbd73844353239bd
anoop chandran <strivetobelazy@gmail.com>
2018-12-13 19:39:53 +0100

    mend

--- shell/git_functions.sh
+++ shell/git_functions.sh
@@ -67,3 +67,10 @@ gc() {
     esac
   done
 }
+#chech how ahead or behind the upstream
+gab() {
+curr_branch=$(git rev-parse --abbrev-ref HEAD);
+curr_remote=$(git config branch.$curr_branch.remote);
+curr_merge_branch=$(git config branch.$curr_branch.merge | cut -d / -f 3);
+git rev-list --left-right --count $curr_branch...$curr_remote/$curr_merge_branch | tr -s '\t' '|';
+}
