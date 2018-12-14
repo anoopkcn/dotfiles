9e15a70ed2f3303490d65dd0246ab26d29ef6373
anoop chandran <strivetobelazy@gmail.com>
2018-12-14 15:45:54 +0100

    diabled backward search key tmux

--- tmux/keys.sh
+++ tmux/keys.sh
@@ -45,11 +45,10 @@ bind-key -T copy-mode-vi v send-keys -X begin-selection
 bind-key -T copy-mode-vi y send-keys -X copy-selection
 bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
 
-# linux
 bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'pbcopy'
 
 
 # search for prompt
-bind-key @ copy-mode\;\
-           send-keys -X start-of-line\;\
-           send-keys -X search-backward "chand@"
+# bind-key @ copy-mode\;\
+#            send-keys -X start-of-line\;\
+#            send-keys -X search-backward "chand@"
