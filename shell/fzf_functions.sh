467523e74dcea478d5e7196445aaa6d8231f7b5d
anoop chandran <strivetobelazy@gmail.com>
2018-12-15 16:05:12 +0100

    fzf git functions implementation

--- shell/fzf_functions.sh
+++ shell/fzf_functions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# Interactive cd
 function icd() {
     if [[ "$#" != 0 ]]; then
         builtin cd "$@";
@@ -16,4 +18,72 @@ function icd() {
         [[ ${#dir} != 0 ]] || return 0
         builtin cd "$dir" &> /dev/null
     done
+}
+
+#----- FZF powerd git functions -------
+function __gi() {
+  curl -L -s https://www.gitignore.io/api/"$@"
+}
+function gitig(){
+	if  [ "$#" -eq 0 ]; then
+		IFS+=","
+		for item in $(__gi list); do
+			echo $item
+		done | fzf --multi --ansi | paste -s -d "," - |
+		{ read result && __gi "$result"; }
+	else
+		__gi "$@"
+	fi
+}
+#
+gitbr() {
+# checkout git branch
+  is_in_git_repo || return
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+
+# checkout git commit with previews
+gitco() {
+  is_in_git_repo || return
+	alias glNoGraph='git log --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr% C(auto)%an" "$@"'
+	_gitLogLineToHash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
+	_viewGitLogLine="$_gitLogLineToHash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy'"
+  local commit
+  commit=$( glNoGraph |
+    fzf --no-sort --reverse --tiebreak=index --no-multi \
+        --ansi --preview="$_viewGitLogLine" ) &&
+  git checkout $(echo "$commit" | sed "s/ .*//")
+}
+# gitstash - easier way to deal with stashes
+# type fstash to get a list of your stashes
+# enter shows you the contents of the stash
+# ctrl-d shows a diff of the stash against your current HEAD
+# ctrl-b checks the stash out as a branch, for easier merging
+gitstash() {
+  is_in_git_repo || return
+  local out q k sha
+  while out=$(
+    git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
+    fzf --ansi --no-sort --query="$q" --print-query \
+        --expect=ctrl-d,ctrl-b);
+  do
+    mapfile -t out <<< "$out"
+    q="${out[0]}"
+    k="${out[1]}"
+    sha="${out[-1]}"
+    sha="${sha%% *}"
+    [[ -z "$sha" ]] && continue
+    if [[ "$k" == 'ctrl-d' ]]; then
+      git diff $sha
+    elif [[ "$k" == 'ctrl-b' ]]; then
+      git stash branch "stash-$sha" $sha
+      break;
+    else
+      git stash show -p $sha
+    fi
+  done
 }
