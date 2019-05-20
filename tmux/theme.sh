ea185d5dcd3b5bab104e13f90826d1741c021261
Anoop Chandran <anoopkcn@gmail.com>
2019-05-20 13:45:23 +0200

    mainly tmux updates

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -17,28 +17,23 @@ set -g status-interval 2
 
 
 # default statusbar colors
-set-option -g status-fg $tm_color_active
-set-option -g status-bg default
-set-option -g status-attr default
+set-option -g status-style fg=$tm_color_active,bg=default,default
 
-# set-option -g status-bg  colour236
 # default window title colors
-set-window-option -g window-status-fg $tm_color_inactive
-set-window-option -g window-status-bg default
+set-window-option -g window-status-style fg=$tm_color_inactive,bg=default
 set -g window-status-format "[#I,#F,#W]"
 
 # active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
-set-window-option -g window-status-current-bg  default
+set-window-option -g window-status-current-style fg=$tm_color_active,bg=default
 set-window-option -g  window-status-current-format "[#I,#F,#W]"
 
 # pane border
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_color_active
+set-option -g pane-border-style fg=$tm_color_inactive
+set-option -g pane-active-border-style fg=$tm_color_active
 
 # message text
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
+set-option -g message-style fg=$tm_color_active,bg=default
+#set-option -g message-fg $tm_color_active
 
 # pane number display
 set-option -g display-panes-active-colour $tm_color_active
@@ -47,10 +42,9 @@ set-option -g display-panes-colour $tm_color_inactive
 # clock
 set-window-option -g clock-mode-colour $tm_color_active
 
-# tm_sysinfo=" #[fg=$tm_color_sysinfo,bright]#(tmux-mem-cpu-load -m 2 -g 0 -a 1)"
 tm_date="#[fg=colour239]%d-%b#[fg=colour239] %l:%M"
 tm_pwd="#[fg=colour239,bold]#{pane_current_path}"
-tm_host="#[fg=$tm_color_feature,bold]@Anoop"
+tm_host="#[fg=$tm_color_feature,bold]#H"
 tm_session_name="#[fg=$tm_color_feature]$tm_separator_left_thin#S"
 # tm_separator_right_thin=" ▣"
 
