c6c7cc58168c8d670b7a17275c2414bc2d1c7028
Anoop Chandran <anoopkcn@gmail.com>
2019-09-13 22:51:55 +0200

    zsh custom completion

--- shell/completions.sh
+++ shell/completions.sh
@@ -1,13 +1,21 @@
-#/usr/bin/env bash
-# BASH completions for custom functions
+# BASH/ZSH completions for custom functions
 
-_sublp_completions()
-{
-    if [ "${#COMP_WORDS[@]}" != "2" ]; then
-        return
-    fi
-    project=$(ls *.sublime-project)
-    COMPREPLY=($(compgen -W "${project%.*}" "${COMP_WORDS[1]}"))
-}
+if ! [ $ZSH_VERSION ]; then
+    _sublp_completions()
+    {
+        if [ "${#COMP_WORDS[@]}" != "2" ]; then
+            return
+        fi
+        project=$(ls *.sublime-project)
+        COMPREPLY=($(compgen -W "${project%.*}" "${COMP_WORDS[1]}"))
+    }
 
-complete -F _sublp_completions sublp
+    complete -F _sublp_completions sublp
+else
+    function _sublp_completions(){
+        project=$(ls *.sublime-project)
+        compadd ${project%.*}
+    }
+
+    compdef _sublp_completions sublp
+fi
