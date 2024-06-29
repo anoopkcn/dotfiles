131e29cc4161f1a92d612f54d4bd56a9a4a47fc4
Anoop K. Chandran <anoopkcn@gmail.com>
2024-06-29 04:44:35 +0200

    simple is better

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-alias ll='ls -al'
 
 # tmux alias
 alias ta='tmux attach -t'
@@ -9,23 +8,9 @@ alias ts='tmuxinator start'
 
 #other
 alias vim='nvim'
-alias workon='conda activate'
-alias workoff='conda deactivate'
 alias duh="du -h -d 0 [^.]*"
-alias js="bundle exec jekyll serve"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   alias pbcopy='xclip -selection clipboard'
   alias pbpaste='xclip -selection clipboard -o'
 fi
-
-# if [[ -d ${HOME}/.homebrew/Cellar/gcc/8.2.0/bin ]]; then
-#   alias gcc='gcc-8'
-#   alias cc='gcc-8'
-#   alias g++='g++-8'
-#   alias c++='c++-8'
-#   alias gfortran='gfortran-8'
-# fi
-
-#Aiida
-# alias vl='verdi process list'
