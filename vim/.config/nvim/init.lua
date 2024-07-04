cb7c785ac5e96e6a7234dede26fb7a86d5f8cce3
Anoop K. Chandran <anoopkcn@gmail.com>
2024-07-04 15:14:17 +0200

    mode lazy setup back to init

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -6,6 +6,22 @@
 require("core.options")
 require("core.keymaps")
 require("core.globals")
+
+-- Lazy plugin manager
+-- https://github.com/folke/lazy.nvim.git
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
 	spec = { { import = "plugins" }, { import = "colors" } },
 	change_detection = { notify = false },
