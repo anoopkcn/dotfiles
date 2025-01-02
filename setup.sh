f3866143502b4f1b37e84289f47ef61d144b2787
Anoop K. Chandran <anoopkcn@gmail.com>
2025-01-02 10:13:42 +0100

    remove starship and add ghostty

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 ln -sf ~/<PATH_TO_DOTFILES>/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/<PATH_TO_DOTFILES>/dotfiles/zsh  ~/.config/zsh
 ln -sf ~/<PATH_TO_DOTFILES>/dotfiles/nvim  ~/.config/nvim
+ln -sf ~/<PATH_TO_DOTFILES>/dotfiles/ghostty  ~/.config/ghostty
 ln -sf ~/<PATH_TO_DOTFILES>/dotfiles/tmux  ~/.config/tmux
-ln -sf ~/<PATH_TO_DOTFILES>/dotfiles/starship.toml ~/.config/starship.toml
-ln -sf ~/<PATH_TO_DOTFILES>/dotfiles/starship ~/.config/starship
