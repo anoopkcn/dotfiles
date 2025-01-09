f34aa44d7f7f880f97e5cf0db75cfb8c1001000d
Anoop K. Chandran <anoopkcn@gmail.com>
2025-01-09 21:28:00 +0100

    use telescope instead of fzflua

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,27 @@
+return {
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+
+	config = function()
+		require('telescope').setup({})
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>gf', builtin.git_files)
+		vim.keymap.set('n', '<leader>gb', builtin.git_branches)
+		vim.keymap.set('n', '<leader>gs', builtin.git_status)
+		vim.keymap.set('n', '<leader>ff', builtin.find_files)
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+		vim.keymap.set('n', '<leader>fb', builtin.buffers)
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+		vim.keymap.set('n', '<leader>fw', function()
+			local word = vim.fn.expand("<cword>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set('n', '<leader>fW', function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({search = word})
+		end)
+		vim.keymap.set('n', '<leader>/', function()
+			builtin.grep_string({search = vim.fn.input("Grep > ")})
+		end)
+	end
+}
