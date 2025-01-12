a8e4746a20d7489742cc153492348c676719aaa1
Anoop K. Chandran <anoopkcn@gmail.com>
2025-01-12 20:55:25 +0100

    refactor fuctions

--- nvim/init.lua
+++ nvim/init.lua
@@ -51,45 +51,6 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setqflist)
 vim.keymap.set('n', '<leader>\\', ':rightbelow vsplit<CR>', { noremap = true, silent = true})
 vim.keymap.set('n', '<leader>-', ':rightbelow split<CR>', { noremap = true, silent = true })
 
--- CUSTOM FUNCTIONS
-Print = function(v)
-	print(vim.inspect(v))
-	return v
-end
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Temporary highlight indicator when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("custom-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
-vim.api.nvim_create_autocmd("TermOpen", {
-	desc = "Vim terminal configurations",
-	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
-	callback = function()
-		vim.opt.number = false
-		vim.opt.relativenumber = false
-	end,
-})
-
--- Command to toggle inline diagnostics
-vim.api.nvim_create_user_command(
-  'DiagnosticsToggleVirtualText',
-  function()
-    local current_value = vim.diagnostic.config().virtual_text
-    if current_value then
-      vim.diagnostic.config({virtual_text = false})
-    else
-      vim.diagnostic.config({virtual_text = true})
-    end
-  end,
-  {}
-)
-
-vim.api.nvim_set_keymap('n', '<Leader>ii', '<cmd>DiagnosticsToggleVirtualText<CR>', { noremap = true, silent = true })
-
 -- PLUGINS
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -122,3 +83,5 @@ require("lazy").setup {
 		{ "numToStr/Comment.nvim" },
 	},
 }
+
+require("custom.functions")
