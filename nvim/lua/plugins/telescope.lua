10c72413b27830dbb97557e59cd6f09ff8724397
Anoop K. Chandran <anoopkcn@gmail.com>
2025-01-16 08:03:50 +0100

    remove trouble

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
 	dependencies = {
-		'nvim-lua/plenary.nvim',
+		"nvim-lua/plenary.nvim",
 		{
 			'nvim-telescope/telescope-fzf-native.nvim',
 			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
@@ -23,6 +23,7 @@ return {
 				git_status = { theme = "ivy", preview_title = false, results_title = false },
 				find_files = { theme = "ivy", preview_title = false, results_title = false },
 				live_grep = { theme = "ivy", preview_title = false, results_title = false },
+				help_tags = { theme = "ivy", preview_title = false, results_title = false },
 				buffers = { theme = "ivy", preview_title = false, results_title = false },
 				grep_string = { theme = "ivy", preview_title = false, results_title = false },
 			},
