5504c7a9f3c10ebe3cb586315726ed8855d8ef27
Anoop K. Chandran <anoopkcn@gmail.com>
2025-01-19 14:42:18 +0100

    fix: make lsp happy

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,12 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require 'nvim-treesitter.configs'.setup {
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "yaml" },
 			sync_install = false,
 			auto_install = false,
+
+			modules = {},
+			ignore_install = {},
 
 			highlight = {
 				enable = true,
