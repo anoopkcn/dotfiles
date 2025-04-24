782f12c7b35a9d399cb9fcbbafb4456f4917dc65
Anoop K. Chandran <anoopkcn@gmail.com>
2025-04-24 02:00:01 +0200

    add checks for setting up path

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-PATH_TO_DOTFILES=$HOME/Dropbox/dotfiles
+
+PATH_TO_DOTFILES=''
+
+if [ -z "$PATH_TO_DOTFILES" ]; then
+  echo "Please set PATH_TO_DOTFILES to the path of your dotfiles directory."
+  exit 1
+fi
+
+# PATH_TO_DOTFILES=$HOME/Dropbox/dotfiles
+
 # ln -sf $PATH_TO_DOTFILES/tmux  $HOME/.config/tmux
 # ln -sf $PATH_TO_DOTFILES/zed  $HOME/.config/zed
 ln -sf $PATH_TO_DOTFILES/ghostty  $HOME/.config/ghostty
