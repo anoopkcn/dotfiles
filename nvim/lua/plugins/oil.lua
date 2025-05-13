ca1f50e415bfc12bda9b7ba1478d86200468fdd6
Anoop K. Chandran <anoopkcn@gmail.com>
2025-05-14 00:41:59 +0200

    add some oil settings

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,11 +1,29 @@
-return{
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+return {
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	-- Optional dependencies
+	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+	config = function()
+		require("oil").setup({
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+				natural_order = true,
+				is_always_hidden = function(name, _)
+					return name == ".git"
+				end,
+			},
+			keymaps = {
+				["<C-c>"] = false,
+				["q"] = "actions.close",
+			},
+		})
+	end
 }
