f70ed503385879bc263ad4723f65718b9c94d3b3
Anoop K. Chandran <anoopkcn@gmail.com>
2025-05-17 21:27:16 +0200

    disable oil fields

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -13,6 +13,13 @@ return {
 			default_file_explorer = true,
 			delete_to_trash = true,
 			skip_confirm_for_simple_edits = true,
+			columns = {
+				-- "icon",
+				-- "type",
+				-- "permissions",
+				-- "size",
+				-- "mtime",
+			},
 			view_options = {
 				show_hidden = true,
 				natural_order = true,
