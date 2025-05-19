d8c43c5ac55cc8fe67046a5d4ae1e563a33fb87a
Anoop K. Chandran <anoopkcn@gmail.com>
2025-05-19 02:14:43 +0200

    move options to a new file

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,64 +3,8 @@
 -- License: MIT
 -- Refer to README.md for more information
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-vim.g.netrw_banner = 0
-
-vim.opt.swapfile = false
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.undofile = true
-vim.opt.cursorline = true
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
--- vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = "a"
-vim.opt.signcolumn = "yes"
-vim.opt.scrolloff = 5
-vim.opt.sidescrolloff = 5
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-vim.opt.linebreak = true
-vim.opt.showmode = false
-vim.opt.termguicolors = true
-vim.opt.showtabline = 0
-
-if vim.fn.executable("rg") == 1 then
-	vim.opt.grepprg = "rg --vimgrep --smart-case"
-	vim.opt.grepformat = "%f:%l:%c:%m"
-end
-
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
-vim.keymap.set({ "n", "v" }, "<C-Space>", "<Nop>")
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "G", "Gzz")
-vim.keymap.set("n", "<C-f>", "<C-f>zz")
-vim.keymap.set("n", "<C-b>", "<C-b>zz")
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
-vim.keymap.set("n", "J", "mzJ`z") -- Join lines without moving cursor
--- vim.keymap.set("n", "<leader>p", [["_dP]])
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
--- vim.keymap.set("n", "j", "v:count ? 'j' : 'gj'", { expr = true })
--- vim.keymap.set("n", "k", "v:count ? 'k' : 'gk'", { expr = true })
-vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
-vim.keymap.set("n", "<leader>on", vim.cmd.only)
-vim.keymap.set("n", "<leader>\\", ":rightbelow vsplit<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>-", ":rightbelow split<CR>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-c>", "<Esc>")
--- lsp + quickfix
-vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
-vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
-vim.keymap.set("n", "<leader>xX", vim.diagnostic.setqflist)
-vim.keymap.set("n", "<leader>xx", vim.diagnostic.setloclist)
-
+require("custom.options")
+require("custom.functions")
 vim.cmd("colorscheme onehalfdark")
 
 -- PLUGINS
@@ -93,8 +37,32 @@ require("lazy").setup {
 	},
 }
 
-require("custom.functions")
-
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
+vim.keymap.set({ "n", "v" }, "<C-Space>", "<Nop>")
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "G", "Gzz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "J", "mzJ`z") -- Join lines without moving cursor
+-- vim.keymap.set("n", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- vim.keymap.set("n", "j", "v:count ? 'j' : 'gj'", { expr = true })
+-- vim.keymap.set("n", "k", "v:count ? 'k' : 'gk'", { expr = true })
+vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
+vim.keymap.set("n", "<leader>on", vim.cmd.only)
+vim.keymap.set("n", "<leader>\\", ":rightbelow vsplit<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>-", ":rightbelow split<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-c>", "<Esc>")
+-- lsp + quickfix
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<leader>xX", vim.diagnostic.setqflist)
+vim.keymap.set("n", "<leader>xx", vim.diagnostic.setloclist)
 
 vim.keymap.set("n", "<leader>q",
 	ToggleQuickfixList,
---
+++ nvim/lua/custom/options.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.netrw_banner = 0
+
+vim.opt.swapfile = false
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.undofile = true
+vim.opt.cursorline = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+-- vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 5
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.linebreak = true
+vim.opt.showmode = false
+vim.opt.termguicolors = true
+vim.opt.showtabline = 0
+
+if vim.fn.executable("rg") == 1 then
+	vim.opt.grepprg = "rg --vimgrep --smart-case"
+	vim.opt.grepformat = "%f:%l:%c:%m"
+end
