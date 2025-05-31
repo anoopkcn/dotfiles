d80675c0692c9f8df5bef7c2a2617a5eff57a031
Anoop K. Chandran <anoopkcn@gmail.com>
2025-05-31 03:07:59 +0200

    remove comments

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,23 +3,12 @@ return {
 	---@module 'oil'
 	---@type oil.SetupOpts
 	opts = {},
-	-- Optional dependencies
-	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
 	config = function()
 		require("oil").setup({
 			default_file_explorer = true,
 			delete_to_trash = true,
 			skip_confirm_for_simple_edits = true,
-			columns = {
-				-- "icon",
-				-- "type",
-				-- "permissions",
-				-- "size",
-				-- "mtime",
-			},
 			view_options = {
 				show_hidden = true,
 				natural_order = true,
