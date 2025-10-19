80a7484b7206608bd4a646182d13e30ffaef01c3
Anoop K. Chandran <anoopkcn@gmail.com>
2025-10-19 15:26:24 +0200

    Remove negit

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -8,6 +8,12 @@ return {
 			default_file_explorer = true,
 			delete_to_trash = true,
 			skip_confirm_for_simple_edits = true,
+			columns = {
+				-- "icon",
+				-- "permissions",
+				-- "size",
+				-- "mtime",
+			},
 			view_options = {
 				show_hidden = true,
 				natural_order = true,
