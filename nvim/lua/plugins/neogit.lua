4e6fca31ef50377d75e5e84b0a5cb52ea297a77a
Anoop K. Chandran <anoopkcn@gmail.com>
2025-10-19 15:47:43 +0200

    Add neogit back

---
+++ nvim/lua/plugins/neogit.lua
@@ -0,0 +1,47 @@
+return {
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+	},
+	config = function()
+		local neogit = require('neogit')
+		neogit.setup({
+			disable_hint = true,
+			graph_style = "kitty",
+			signs = {
+				hunk = { "", "" },
+				item = { "", "" },
+				section = { "", "" },
+			},
+		})
+
+		vim.keymap.set(
+			"n",
+			"<leader>gg",
+			"<CMD>Neogit kind=replace<CR>",
+			{ noremap = true, silent = true, desc = "Open Neogit interface" }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>gl",
+			function() neogit.open({ "log" }) end,
+			{ noremap = true, silent = true, desc = "Git log" }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>gx",
+			"<CMD>!git checkout -- %<CR>",
+			{ noremap = true, silent = true, desc = "Discard changes in current file" }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>gc",
+			function() neogit.action("commit", "commit")() end,
+			{ noremap = true, silent = true, desc = "Commit" }
+		)
+	end
+}
