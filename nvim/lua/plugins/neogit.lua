2e61ad65ddc7c46fc5fac0263f69bab1d13b11ae
Anoop K. Chandran <anoopkcn@gmail.com>
2025-10-19 12:50:58 +0200

    Ad descriptions and format file

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -10,28 +10,52 @@ return {
 			disable_hint = true,
 			graph_style = "kitty",
 			signs = {
-				-- CLOSED, OPENED
 				hunk = { "", "" },
 				item = { "", "" },
 				section = { "", "" },
 			},
 		})
 
-		vim.keymap.set("n", "<leader>gg", "<CMD>Neogit kind=replace<CR>", { noremap = true, silent = true })
-		-- vim.keymap.set("n", "<leader>g", "<CMD>Neogit kind=split_below_all<CR>", { noremap = true, silent = true })
-		-- vim.keymap.set("n", "<leader>l", function() neogit.action("log", "log_current")() end)
-		vim.keymap.set("n", "<leader>gl", function() neogit.open({"log"}) end)
+		vim.keymap.set(
+			"n",
+			"<leader>gg",
+			"<CMD>Neogit kind=replace<CR>",
+			{ noremap = true, silent = true, desc = "Open Neogit interface" }
+		)
 
-		-- Stage current file
-		vim.keymap.set("n", "<leader>gs", function() require('gitsigns').stage_buffer() end, { noremap = true, silent = true, desc = "Stage current file" })
+		vim.keymap.set(
+			"n",
+			"<leader>gl",
+			function() neogit.open({ "log" }) end,
+			{ noremap = true, silent = true, desc = "Git log" }
+		)
 
-		-- Unstage current file
-		vim.keymap.set("n", "<leader>gu", function() require('gitsigns').reset_buffer_index() end, { noremap = true, silent = true, desc = "Unstage current file" })
+		vim.keymap.set(
+			"n",
+			"<leader>gs",
+			function() require('gitsigns').stage_buffer() end,
+			{ noremap = true, silent = true, desc = "Stage current file" }
+		)
 
-		-- Discard changes in current file
-		vim.keymap.set("n", "<leader>gx", "<CMD>!git checkout -- %<CR>", { noremap = true, silent = true, desc = "Discard changes in current file" })
+		vim.keymap.set(
+			"n",
+			"<leader>gu",
+			function() require('gitsigns').reset_buffer_index() end,
+			{ noremap = true, silent = true, desc = "Unstage current file" }
+		)
 
-		-- Commit
-		vim.keymap.set("n", "<leader>gc", function() neogit.action("commit", "commit")() end, { noremap = true, silent = true, desc = "Commit" })
+		vim.keymap.set(
+			"n",
+			"<leader>gx",
+			"<CMD>!git checkout -- %<CR>",
+			{ noremap = true, silent = true, desc = "Discard changes in current file" }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>gc",
+			function() neogit.action("commit", "commit")() end,
+			{ noremap = true, silent = true, desc = "Commit" }
+		)
 	end
 }
