c3a7dd66068cf5fc63660933a56a3f2e628a087f
Anoop K. Chandran <anoopkcn@gmail.com>
2025-10-21 01:02:45 +0200

    Simpler maps

--- nvim/lua/plugins/gitsignes.lua
+++ nvim/lua/plugins/gitsignes.lua
@@ -29,15 +29,15 @@ return {
 				end)
 
 				-- Actions
-				map('n', '<leader>hs', gitsigns.stage_hunk)
-				map('n', '<leader>hr', gitsigns.reset_hunk)
-				map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-				map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-				map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-				map('n', '<leader>hv', gitsigns.preview_hunk_inline)
-				map('n', '<leader>hb', gitsigns.blame_line)
-				map('n', '<leader>hn', function() gitsigns.nav_hunk("next") end)
-				map('n', '<leader>hp', function() gitsigns.nav_hunk("prev") end)
+				vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk, { noremap = true, silent = true, desc = "Stage hunk" })
+				vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk, { noremap = true, silent = true, desc = "Reset hunk" })
+				vim.keymap.set('n', '<leader>hu', gitsigns.undo_stage_hunk, { noremap = true, silent = true, desc = "Undo stage hunk" })
+				vim.keymap.set('n', '<leader>hv', gitsigns.preview_hunk_inline, { noremap = true, silent = true, desc = "Preview hunk inline" })
+				vim.keymap.set('n', '<leader>hb', gitsigns.blame_line, { noremap = true, silent = true, desc = "Blame line" })
+				-- vim.keymap.set('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+				-- vim.keymap.set('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+				-- vim.keymap.set('n', '<leader>hn', function() gitsigns.nav_hunk("next") end)
+				-- vim.keymap.set('n', '<leader>hp', function() gitsigns.nav_hunk("prev") end)
 			end
 		})
 	end
