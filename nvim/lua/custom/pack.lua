f55e98d8d64857874dcbeb82afd370a04f288fd9
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-10 20:02:06 +0100

    Restructure the package manager a bit

--- nvim/lua/custom/pack.lua
+++ nvim/lua/custom/pack.lua
@@ -1,50 +1,14 @@
 local M = {}
+local plugin_specs
+local plugin_configs
 
-local plugin_specs = {
-    -- LSP
-    "https://github.com/mason-org/mason.nvim",
-    "https://github.com/mason-org/mason-lspconfig.nvim",
-    "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
-    "https://github.com/nvim-treesitter/nvim-treesitter",
-    -- Autompletion
-    "https://github.com/hrsh7th/nvim-cmp",
-    "https://github.com/hrsh7th/cmp-nvim-lsp",
-    "https://github.com/hrsh7th/cmp-path",
-    "https://github.com/hrsh7th/cmp-buffer",
-    -- Debugging
-    "https://github.com/mfussenegger/nvim-dap",
-    "https://github.com/rcarriga/nvim-dap-ui",
-    "https://github.com/nvim-neotest/nvim-nio",
-    "https://github.com/mfussenegger/nvim-dap-python",
-    -- tpope
-    "https://github.com/tpope/vim-fugitive",
-    "https://github.com/tpope/vim-surround",
-    "https://github.com/tpope/vim-unimpaired",
-    "https://github.com/tpope/vim-repeat",
-    "https://github.com/github/copilot.vim", -- yes, it's from tpope
-    -- Utilities
-    "https://github.com/lewis6991/gitsigns.nvim",
-    "https://github.com/ibhagwan/fzf-lua",
-    "https://github.com/stevearc/oil.nvim",
-}
+function M.setup()
+    if not vim.pack or not vim.pack.add then
+        vim.notify("vim.pack is unavailable in this version of Neovim", vim.log.levels.ERROR)
+        return
+    end
 
-local plugin_configs = {
-    "plugins.mason",
-    "plugins.cmp",
-    "plugins.copilot",
-    "plugins.dap",
-    "plugins.fugitive",
-    "plugins.fzf",
-    "plugins.gitsigns",
-    "plugins.oil",
-    "plugins.treesitter",
-    "plugins.lsp",
-    "plugins.makepicker"
-}
-
-local function setup_pack_hooks()
     local group = vim.api.nvim_create_augroup("CustomPackHooks", { clear = true })
-
     vim.api.nvim_create_autocmd("PackChanged", {
         group = group,
         callback = function(ev)
@@ -64,26 +28,69 @@ local function setup_pack_hooks()
             end
         end,
     })
-end
 
-function M.setup()
-    if not vim.pack or not vim.pack.add then
-        vim.notify("vim.pack is unavailable in this version of Neovim", vim.log.levels.ERROR)
-        return
-    end
-
-    setup_pack_hooks()
     vim.pack.add(plugin_specs, {
         confirm = false,
         load = true,
     })
 
-    for _, module_name in ipairs(plugin_configs) do
+    local iter = vim.iter
+    local function setup_plugin(module_name)
         local ok, mod = pcall(require, module_name)
         if ok and type(mod.setup) == "function" then
             mod.setup()
         end
     end
+
+    if iter then
+        iter(plugin_configs):each(setup_plugin)
+    else
+        for _, module_name in ipairs(plugin_configs) do
+            setup_plugin(module_name)
+        end
+    end
 end
+
+plugin_specs = {
+    -- LSP
+    "mason-org/mason.nvim",
+    "mason-org/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    -- Autompletion
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    -- Debugging
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "mfussenegger/nvim-dap-python",
+    -- tpope
+    "tpope/vim-fugitive",
+    "tpope/vim-surround",
+    "tpope/vim-unimpaired",
+    "tpope/vim-repeat",
+    "github/copilot.vim", -- yes, it's from tpope
+    -- Utilities
+    "lewis6991/gitsigns.nvim",
+    "ibhagwan/fzf-lua",
+    "stevearc/oil.nvim",
+}
+
+plugin_configs = {
+    "plugins.mason",
+    "plugins.cmp",
+    "plugins.copilot",
+    "plugins.dap",
+    "plugins.fugitive",
+    "plugins.fzf",
+    "plugins.gitsigns",
+    "plugins.oil",
+    "plugins.treesitter",
+    "plugins.lsp",
+    "plugins.makepicker"
+}
 
 return M
