604149efc68d58513bcbbba8e34b53ee2d820522
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-12 02:20:59 +0100

    add grug keymaps

--- nvim/lua/plugins/grug.lua
+++ nvim/lua/plugins/grug.lua
@@ -13,9 +13,49 @@ function M.setup()
     if ok_grug then
         grug.setup({
             windowCreationCommand = "rightbelow vsplit",
+            helpLine = {
+                enabled = false,
+            },
         })
     end
-    vim.keymap.set("n", "<leader>r", "<cmd>GrugFar<cr>", { desc = "Find in files with grug" })
+
+    vim.keymap.set({ "n", "v" }, "<leader>sr", function()
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+            transient = true,
+            prefills = {
+                filesFilter = (ext and ext ~= "") and ("*." .. ext) or nil,
+            },
+        })
+    end, { desc = "Search and replace (grug)" })
+
+    vim.keymap.set("n", "<leader>sw", function()
+        grug.open({
+            transient = true,
+            prefills = {
+                search = vim.fn.expand("<cword>"),
+            },
+        })
+    end, { desc = "Search word under cursor (grug)" })
+
+    vim.keymap.set("n", "<leader>sf", function()
+        grug.open({
+            transient = true,
+            prefills = {
+                paths = vim.fn.expand("%"),
+            },
+        })
+    end, { desc = "Search current file (grug)" })
+
+    vim.keymap.set("v", "<leader>sv", function()
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.with_visual_selection({
+            transient = true,
+            prefills = {
+                filesFilter = (ext and ext ~= "") and ("*." .. ext) or nil,
+            },
+        })
+    end, { desc = "Search visual selection in matching files (grug)" })
 end
 
 return M
