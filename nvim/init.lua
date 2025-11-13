68d2f55bc4384595f5d77b3b1570a3f25622b515
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-13 15:34:57 +0100

    pretty rearrangement

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,33 +1,33 @@
--- AUTHOR:  @anoopkcn
 -- LICENSE: MIT
+-- AUTHOR:  @anoopkcn
 -- WARNING: Since vim/neovim can execute any shell command,
--- it is the resposibility of the user to read/check
+-- it is the resposibility of the user to read/check/inspect
 -- the 3rd party plugins to make sure malicious codes doesn't exist.
 -- NOTE: When removing a plugin also remove it from nvim-pack-lock.json
 
 require("custom.options")
-require("custom.functions")
 require("custom.keymaps")
+require("custom.functions")
 require("custom.statusline")
 require("custom.pack").ensure_and_setup(
     {
         -- plugins without setup
-        "https://github.com/tpope/vim-surround",
-        "https://github.com/tpope/vim-unimpaired",
         "https://github.com/tpope/vim-repeat",
         "https://github.com/tpope/vim-rhubarb",
+        "https://github.com/tpope/vim-surround",
+        "https://github.com/tpope/vim-unimpaired",
     },
     {
         -- plugins with setup and configs
         "plugins.mason",
         "plugins.lsp",
         "plugins.fzf",
-        "plugins.treesitter",
-        "plugins.fugitive",
-        "plugins.minidiff",
-        "plugins.copilot",
         "plugins.grug",
         "plugins.blink",
+        "plugins.copilot",
+        "plugins.fugitive",
+        "plugins.minidiff",
+        "plugins.treesitter",
         "plugins.makepicker",
         -- "plugins.dap",
     }
