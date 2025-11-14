ae226cc77c814dfa4dd53e666b26e2136e7545c6
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-14 18:22:27 +0100

    Add configuration documentation

--- nvim/lsp/marksman.lua
+++ nvim/lsp/marksman.lua
@@ -1,3 +1,13 @@
+---@brief
+---
+--- https://github.com/artempyanykh/marksman
+---
+--- Marksman is a Markdown LSP server providing completion, cross-references, diagnostics, and more.
+---
+--- Marksman works on MacOS, Linux, and Windows and is distributed as a self-contained binary for each OS.
+---
+--- Pre-built binaries can be downloaded from https://github.com/artempyanykh/marksman/releases
+
 return {
     cmd = { "marksman", "server" },
     filetypes = { "markdown" },
