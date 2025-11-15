dfe334e7e2a444130827a56c1558f9dc744731a2
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-15 21:03:59 +0100

    Do not auto comment

--- nvim/lua/core/auto.lua
+++ nvim/lua/core/auto.lua
@@ -38,12 +38,12 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 })
 
 -- no auto continue comments on new line
--- vim.api.nvim_create_autocmd("FileType", {
--- 	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
--- 	callback = function()
--- 		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
--- 	end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
 
 -- show cursorline only in active window enable
 vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
