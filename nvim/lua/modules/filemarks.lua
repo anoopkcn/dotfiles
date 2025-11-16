98e80549afb5bf1c4b4488fadc3a3d09d0edc238
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-16 23:09:05 +0100

    Move usercommands to plugins and add modifiable marks list

--- nvim/lua/modules/filemarks.lua
+++ nvim/lua/modules/filemarks.lua
@@ -1,4 +1,3 @@
--- Local filemarks plugin bindings and commands.
 local M = {}
 
 function M.setup()
@@ -8,42 +7,6 @@ function M.setup()
     end
 
     filemarks.setup()
-
-    vim.api.nvim_create_user_command("FilemarksAdd", function(opts)
-        local key = opts.fargs[1]
-        local target = opts.fargs[2]
-        filemarks.add(key, target)
-    end, {
-        desc = "Add/update a persistent filemark",
-        nargs = "*",
-        complete = "file",
-    })
-
-    vim.api.nvim_create_user_command("FilemarksRemove", function(opts)
-        local key = opts.fargs[1]
-        filemarks.remove(key)
-    end, {
-        desc = "Remove a filemark from the current project",
-        nargs = "?",
-    })
-
-    vim.api.nvim_create_user_command("FilemarksList", function()
-        filemarks.list()
-    end, {
-        desc = "List filemarks for the current project",
-    })
-
-    vim.api.nvim_create_user_command("FilemarksOpen", function(opts)
-        local key = opts.fargs[1]
-        if not key then
-            vim.notify("Filemarks: provide a key to open", vim.log.levels.WARN)
-            return
-        end
-        filemarks.open(key)
-    end, {
-        desc = "Jump to the file referenced by a key",
-        nargs = 1,
-    })
 end
 
 return M
