3a6871f1561dacf14389a9db3d7d8418d62ff119
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-16 11:47:02 +0100

    Add back some stuff

--- nvim/lua/core/auto.lua
+++ nvim/lua/core/auto.lua
@@ -20,6 +20,30 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
+-- show cursorline only in active window enable
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+    group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+    callback = function()
+        vim.opt_local.cursorline = true
+    end,
+})
+
+-- show cursorline only in active window disable
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+    group = "active_cursorline",
+    callback = function()
+        vim.opt_local.cursorline = false
+    end,
+})
+
+-- no auto continue comments on new line
+-- vim.api.nvim_create_autocmd("FileType", {
+--     group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+--     callback = function()
+--         vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+--     end,
+-- })
+
 -- restore cursor to file position in previous editing session
 vim.api.nvim_create_autocmd("BufReadPost", {
     callback = function(args)
