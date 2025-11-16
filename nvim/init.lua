1b035d2041127aac193a0626139c5284c2828fe2
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-16 11:28:06 +0100

    No more almonds

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,29 +5,28 @@ require("core.auto")
 require("core.options")
 require("core.keymaps")
 require("core.statusline")
-require("core.pack").ensure(
-    {
-        "https://github.com/tpope/vim-repeat",
-        "https://github.com/tpope/vim-surround",
-        "https://github.com/tpope/vim-unimpaired"
-    },
-    {
-        "modules.fzf",
-        "modules.blink",
-        "modules.cgrep",
-        "modules.quicker",
-        "modules.fugitive",
-        "modules.minidiff",
-        "modules.treesitter",
-        "modules.makepicker"
-    }
-)
+local pack = require("core.pack")
+local specs = {
+    "https://github.com/tpope/vim-repeat",
+    "https://github.com/tpope/vim-surround",
+    "https://github.com/tpope/vim-unimpaired"
+}
+local modules = {
+    "modules.fzf",
+    "modules.blink",
+    "modules.cgrep",
+    "modules.quicker",
+    "modules.fugitive",
+    "modules.minidiff",
+    "modules.treesitter",
+    "modules.makepicker"
+}
+local lsps = {
+    "lua_ls",
+    "pyright",
+    "copilot",
+    "marksman"
+}
 
-vim.lsp.enable(
-    {
-        "lua_ls",
-        "pyright",
-        "copilot",
-        "marksman"
-    }
-)
+pack.ensure(specs, modules)
+vim.lsp.enable(lsps)
