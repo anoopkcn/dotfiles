d576a38f4625246a572c60e687e623d3952b9b29
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-18 20:02:45 +0100

    Set float and onmi for onehalfdark

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -78,7 +78,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- end
         local map_opts = { buffer = bufnr, noremap = true, silent = true }
         vim.keymap.set("n", "K", function()
-            vim.lsp.buf.hover { max_height = 25, max_width = 100 }
+            vim.lsp.buf.hover { max_height = 25, max_width = 100, border = "rounded" }
         end, map_opts)
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, map_opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts)
