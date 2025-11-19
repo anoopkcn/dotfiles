8d18766a2ecbb146616bbd8028dafe8231c919f7
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-19 19:45:36 +0100

    Show diffs in quickfixlist

--- nvim/lua/modules/minidiff.lua
+++ nvim/lua/modules/minidiff.lua
@@ -11,6 +11,17 @@ M.config = function()
                 signs = { add = "░", change = "░", delete = "░" },
             }
         })
+
+      vim.keymap.set("n", "<leader>fg", function()
+          local hunks = minidiff.export('qf')
+          if #hunks == 0 then
+              vim.notify("No changes to show", vim.log.levels.INFO)
+              return
+          end
+          vim.fn.setqflist(hunks)
+          vim.cmd("copen")
+      end, { desc = "Diff: Open quickfix with all hunks" })
+
     end
 end
 
