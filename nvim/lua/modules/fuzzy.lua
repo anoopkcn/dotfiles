34a1f6532547567b5b2e1a3f8a71487b3b6290b9
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-20 14:09:17 +0100

    Feat select from listed qf lists

--- nvim/lua/modules/fuzzy.lua
+++ nvim/lua/modules/fuzzy.lua
@@ -24,6 +24,8 @@ M.config = function()
         fuzzy.grep(args)
     end
 
+    vim.keymap.set("n", "<leader>ql", "<CMD>FuzzyQf<CR>",
+        { silent = false, desc = "Fuzzy Quickfixlist list" })
     vim.keymap.set("n", "<leader>/", "<CMD>FuzzyGrep<CR>",
         { silent = false, desc = "Fuzzy grep - same as rg (FuzzyGrep)" })
     vim.keymap.set("n", "<leader>fw", function()
