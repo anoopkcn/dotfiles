21bc1ceb1ad990bee16dbcdca0058db1e4138880
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-21 00:24:48 +0100

    Optmized autocomands

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -19,7 +19,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- no auto continue comments on new line
 vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+    group = vim.api.nvim_create_augroup("no_auto_comment", { clear = true }),
     callback = function()
         vim.opt_local.formatoptions:remove({ "c", "r", "o" })
     end,
@@ -27,6 +27,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- restore cursor to file position in previous editing session
 vim.api.nvim_create_autocmd("BufReadPost", {
+    group = vim.api.nvim_create_augroup("restore_cursor", { clear = true }),
     callback = function(args)
         local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
         local line_count = vim.api.nvim_buf_line_count(args.buf)
@@ -40,43 +41,47 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 })
 
 -- custom LSP attach actions
-local _group = vim.api.nvim_create_augroup("CustomLspAttach", { clear = true })
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = _group,
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("CustomLspAttach", { clear = true }),
     callback = function(args)
         local bufnr = args.buf
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        if client then
-            local provider = client.server_capabilities and client.server_capabilities.semanticTokensProvider
-            if provider then
-                if provider.full or provider.range then
-                    client.server_capabilities.semanticTokensProvider = nil
-                end
-            end
 
-            -- copilot inline suggestions
-            if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
-                vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
-
-                vim.keymap.set("i", "<Tab>", function()
-                    return vim.lsp.inline_completion.get() and "" or "<Tab>"
-                end, { desc = "LSP: accept inline completion", buffer = bufnr, expr = true, }
-                )
-                vim.keymap.set("i", "<C-G>", vim.lsp.inline_completion.select,
-                    { desc = "LSP: switch inline completion", buffer = bufnr }
-                )
-            end
-
-            -- lsp autotriggered completion
-            if client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-                vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-                vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
-                vim.keymap.set('i', '<C-Space>', function() vim.lsp.completion.get() end)
-            end
+        -- Disable semantic tokens if supported
+        local semantic_provider = client.server_capabilities and client.server_capabilities.semanticTokensProvider
+        if semantic_provider and (semantic_provider.full or semantic_provider.range) then
+            client.server_capabilities.semanticTokensProvider = nil
         end
+
+        -- Copilot inline suggestions
+        if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
+            vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
+
+            vim.keymap.set("i", "<Tab>", function()
+                return vim.lsp.inline_completion.get() and "" or "<Tab>"
+            end, {
+                desc = "LSP: accept inline completion",
+                buffer = bufnr,
+                expr = true,
+            })
+
+            vim.keymap.set("i", "<C-G>", vim.lsp.inline_completion.select, {
+                desc = "LSP: switch inline completion",
+                buffer = bufnr,
+            })
+        end
+
+        -- LSP autotriggered completion
+        if client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+            vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+            vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+            vim.keymap.set("i", "<C-Space>", vim.lsp.completion.get)
+        end
+
         local map_opts = { buffer = bufnr, noremap = true, silent = true }
-        local hover_opts = { max_height = 25, max_width = 100, border = "rounded" }
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover(hover_opts) end, map_opts)
+        vim.keymap.set("n", "K", function()
+            vim.lsp.buf.hover({ max_height = 25, max_width = 100, border = "rounded" })
+        end, map_opts)
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, map_opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts)
     end,
