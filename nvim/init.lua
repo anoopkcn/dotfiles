533f1af742d3df7c78eb22cc0e603408db39e1ff
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-21 13:33:57 +0100

    Make small neovim packages out of  my plugins and move them out of the configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,25 +1,28 @@
 -- LICENSE: MIT
 -- AUTHOR:  @anoopkcn
 vim.pack.add({
+    { src = "https://github.com/anoopkcn/fuzzy.nvim" },
+    { src = "https://github.com/anoopkcn/filemarks.nvim" },
+    { src = "https://github.com/anoopkcn/statusline.nvim" },
     { src = "https://github.com/tpope/vim-repeat" },
     { src = "https://github.com/tpope/vim-surround" },
     { src = "https://github.com/tpope/vim-fugitive" },
     { src = "https://github.com/tpope/vim-unimpaired" },
     { src = "https://github.com/nvim-mini/mini.diff" },
-    { src = "https://github.com/stevearc/quicker.nvim", version="master" },
+    { src = "https://github.com/stevearc/quicker.nvim", version = "master" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
 require("core.options")
 require("core.keymaps")
 require("core.autocmds")
-require("core.fuzzy")
-require("core.filemarks")
-require("core.statusline")
 
+require("modules.fuzzy")
 require("modules.quicker")
 require("modules.minidiff")
+require("modules.filemarks")
 require("modules.treesitter")
+require("modules.statusline")
 
 vim.lsp.enable({
     "lua_ls",
