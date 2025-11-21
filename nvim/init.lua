9550a75ddd8335c644ff4c19f4074ffa8cf105f7
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-21 14:21:46 +0100

    Give plugins a name so that when calling update it is easier to identify

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,16 @@
 -- LICENSE: MIT
 -- AUTHOR:  @anoopkcn
 vim.pack.add({
-    { src = "https://github.com/anoopkcn/fuzzy.nvim" },
-    { src = "https://github.com/anoopkcn/filemarks.nvim" },
-    { src = "https://github.com/anoopkcn/statusline.nvim" },
-    { src = "https://github.com/tpope/vim-repeat" },
-    { src = "https://github.com/tpope/vim-surround" },
-    { src = "https://github.com/tpope/vim-fugitive" },
-    { src = "https://github.com/tpope/vim-unimpaired" },
-    { src = "https://github.com/nvim-mini/mini.diff" },
-    { src = "https://github.com/stevearc/quicker.nvim", version = "master" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+    { src = "https://github.com/anoopkcn/fuzzy.nvim", name = "fuzzy" },
+    { src = "https://github.com/anoopkcn/filemarks.nvim", name = "filemarks" },
+    { src = "https://github.com/anoopkcn/statusline.nvim", name = "statusline" },
+    { src = "https://github.com/tpope/vim-repeat", name = "vim-repeat" },
+    { src = "https://github.com/tpope/vim-surround", name = "vim-surround" },
+    { src = "https://github.com/tpope/vim-fugitive", name = "vim-fugitive" },
+    { src = "https://github.com/tpope/vim-unimpaired", name = "vim-unimpaired" },
+    { src = "https://github.com/nvim-mini/mini.diff", name = "mini.diff", version = "main" },
+    { src = "https://github.com/stevearc/quicker.nvim", name ="quicker", version = "master" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", name ="treesitter", version = "main" },
 })
 
 require("core.options")
