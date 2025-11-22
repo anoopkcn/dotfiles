62e4f1f01ce16223e7cd98b679d4743910c98d7a
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-22 22:45:36 +0100

    open filemarks in a split

--- nvim/lua/modules/filemarks.lua
+++ nvim/lua/modules/filemarks.lua
@@ -1,4 +1,10 @@
 local ok, filemarks = pcall(require, "filemarks")
 if ok then
     filemarks.setup()
+    local function open_filemarks_list()
+        vim.cmd("split")
+        vim.cmd("FilemarksList")
+    end
+    vim.keymap.set("n", "<leader>l", open_filemarks_list,
+        { noremap = true, silent = true, desc = "show file marks list" })
 end
