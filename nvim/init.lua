b27d462b7b8940c56dce99865876dc0b15894301
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-22 04:15:48 +0100

    Move packages to a new file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,9 @@
 -- LICENSE: MIT
 -- AUTHOR:  @anoopkcn
-vim.pack.add({
-    { src = "/Users/akc/develop/fuzzy.nvim", name = "fuzzy" },
-    { src = "/Users/akc/develop/filemarks.nvim", name = "filemarks" },
-    { src = "/Users/akc/develop/statusline.nvim", name = "statusline" },
-    { src = "https://github.com/tpope/vim-repeat", name = "vim-repeat" },
-    { src = "https://github.com/tpope/vim-surround", name = "vim-surround" },
-    { src = "https://github.com/tpope/vim-fugitive", name = "vim-fugitive" },
-    { src = "https://github.com/tpope/vim-unimpaired", name = "vim-unimpaired" },
-    { src = "https://github.com/nvim-mini/mini.diff", name = "mini.diff", version = "main" },
-    { src = "https://github.com/stevearc/quicker.nvim", name ="quicker", version = "master" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", name ="treesitter", version = "main" },
-})
-
 require("core.options")
 require("core.keymaps")
 require("core.autocmds")
-
+require("core.packages")
 require("modules.fuzzy")
 require("modules.quicker")
 require("modules.minidiff")
@@ -24,4 +11,9 @@ require("modules.filemarks")
 require("modules.treesitter")
 require("modules.statusline")
 
-vim.lsp.enable({ "lua_ls", "pyright", "copilot", "marksman" })
+vim.lsp.enable({
+    "lua_ls",
+    "pyright",
+    "copilot",
+    "marksman"
+})
---
+++ nvim/lua/core/packages.lua
@@ -0,0 +1,45 @@
+vim.pack.add({
+    {
+        src = "/Users/akc/develop/fuzzy.nvim",
+        name = "fuzzy"
+    },
+    {
+        src = "/Users/akc/develop/filemarks.nvim",
+        name = "filemarks"
+    },
+    {
+        src = "/Users/akc/develop/statusline.nvim",
+        name = "statusline"
+    },
+    {
+        src = "https://github.com/tpope/vim-repeat",
+        name = "vim-repeat"
+    },
+    {
+        src = "https://github.com/tpope/vim-surround",
+        name = "vim-surround"
+    },
+    {
+        src = "https://github.com/tpope/vim-fugitive",
+        name = "vim-fugitive"
+    },
+    {
+        src = "https://github.com/tpope/vim-unimpaired",
+        name = "vim-unimpaired"
+    },
+    {
+        src = "https://github.com/nvim-mini/mini.diff",
+        name = "mini.diff",
+        version = "main"
+    },
+    {
+        src = "https://github.com/stevearc/quicker.nvim",
+        name = "quicker",
+        version = "master"
+    },
+    {
+        src = "https://github.com/nvim-treesitter/nvim-treesitter",
+        name = "treesitter",
+        version = "main"
+    },
+})
