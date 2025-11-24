4f156ef9b114816b5a157ae59202683d1350a391
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-24 17:45:56 +0100

    Simplify setup

--- setup.sh
+++ setup.sh
@@ -4,19 +4,16 @@
 # It exist as a reference to my dotfiles.
 
 
-PATH_TO_DOTFILES=''
+PATH_TO_DOTFILES='/Users/akc/develop/dotfiles'
 
 if [ -z "$PATH_TO_DOTFILES" ]; then
   echo "PATH_TO_DOTFILES is not set."
   exit 1
 fi
 
-# PATH_TO_DOTFILES=$HOME/Dropbox/dotfiles
-
-# ln -sf $PATH_TO_DOTFILES/tmux  $HOME/.config/tmux
-# ln -sf $PATH_TO_DOTFILES/zed  $HOME/.config/zed
 ln -sf $PATH_TO_DOTFILES/ghostty  $HOME/.config/ghostty
 ln -sf $PATH_TO_DOTFILES/nvim  $HOME/.config/nvim
 ln -sf $PATH_TO_DOTFILES/zsh  $HOME/.config/zsh
 ln -sf $PATH_TO_DOTFILES/zsh/zshrc $HOME/.zshrc
-source $HOME/.zshrc
+
+# Re-open the terminal
