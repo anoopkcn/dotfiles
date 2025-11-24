7aeaf204da77ff7b5aca54447b6281532b44947a
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-24 22:37:39 +0100

    Set some default for oil

--- nvim/lua/modules/oil.lua
+++ nvim/lua/modules/oil.lua
@@ -1,10 +1,23 @@
 local ok, oil = pcall(require, "oil")
 if ok then
     oil.setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
         columns = {
             "permissions",
             "size",
             "mtime",
+        },
+        view_options = {
+            show_hidden = true,
+            natural_order = true,
+        },
+        confirmation = {
+            border = "rounded",
+        },
+        keymaps = {
+            ["<C-c>"] = false,
+            ["q"] = "actions.close",
         },
     })
     vim.keymap.set("n", "<leader>fe", function() oil.open() end, { desc = "Open parent directory" })
