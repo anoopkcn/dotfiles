93a13f7322b898536b832668300b0e62a5e0aed4
Anoop K. Chandran <anoopkcn@gmail.com>
2025-11-24 17:35:21 +0100

    Some of the remaps are remapped

--- nvim/lua/modules/fuzzy.lua
+++ nvim/lua/modules/fuzzy.lua
@@ -13,31 +13,10 @@ if ok then
         fuzzy.grep(args)
     end
 
-    local function _fuzzy_help()
-        local pattern = vim.fn.input("FH: ")
-        if vim.trim(pattern) == "" then
-            return
-        end
-        local help_dirs = vim.api.nvim_get_runtime_file("doc/", true)
-        if #help_dirs == 0 then
-            vim.notify("Nvim runtime doc/ directories not found.", vim.log.levels.INFO)
-            return
-        end
-        local args = { pattern, "--type", "txt" }
-        vim.list_extend(args, help_dirs)
-        fuzzy.grep(args)
-    end
-
-    vim.keymap.set("n", "<leader>fl", "<CMD>FuzzyList<CR>",
-        { silent = false, desc = "Fuzzy list" })
-
-    vim.keymap.set("n", "<leader>fh", _fuzzy_help,
-        { silent = false, desc = "Fuzzy grep vim help docs" })
-
-    vim.keymap.set("n", "<leader>/", "<CMD>FuzzyGrep<CR>",
+    vim.keymap.set("n", "<leader>/", ":FG! ",
         { silent = false, desc = "Fuzzy grep - same as rg (FuzzyGrep)" })
 
-    vim.keymap.set("n", "<leader>fg", "<CMD>FuzzyGrep!<CR>",
+    vim.keymap.set("n", "<leader>?", ":FF! ",
         { silent = false, desc = "Fuzzy grep deduplicated - same as rg (FuzzyGrep)" })
 
     vim.keymap.set("n", "<leader>fw", function() _fuzzy_grep(vim.fn.expand("<cword>"), false) end,
@@ -46,8 +25,8 @@ if ok then
     vim.keymap.set("n", "<leader>fW", function() _fuzzy_grep(vim.fn.expand("<cWORD>"), true) end,
         { silent = false, desc = "Fuzzy grep current WORD" })
 
-    vim.keymap.set("n", "<leader>ff", "<CMD>FuzzyFiles!<CR>",
-        { noremap = true, silent = true, desc = "Fuzzy find files (FuzzyFiles)" })
+    vim.keymap.set("n", "<leader>fl", "<CMD>FuzzyList<CR>",
+        { silent = false, desc = "Fuzzy list" })
 
     vim.keymap.set("n", "<leader>fb", "<CMD>FuzzyBuffers<CR>",
         { noremap = true, silent = true, desc = "Fuzzy buffer list" })
