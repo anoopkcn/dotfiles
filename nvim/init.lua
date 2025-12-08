d61ac45366d137d7dc4c392b6bb57587cd3189d2
Anoop K. Chandran <anoopkcn@gmail.com>
2025-12-08 02:58:08 +0100

    Simplify the fuzzy keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -180,24 +180,23 @@ end
 local ok, fuzzy = pcall(require, "fuzzy")
 if ok then
     fuzzy.setup()
-    local function _fuzzy_grep(term, literal)
-        term = vim.trim(term or "")
-        if term == "" then return end
-        local args = { term }
-        if literal then table.insert(args, 1, "-F") end
-        fuzzy.grep(args)
-    end
 
     -- vim.keymap.set('n', ']q', '<CMD>FuzzyNext<CR>')
     -- vim.keymap.set('n', '[q', '<CMD>FuzzyPrev<CR>')
+    vim.keymap.set("n", "<leader>fl", "<CMD>FuzzyList<CR>", { silent = false, desc = "Fuzzy list" })
     vim.keymap.set("n", "<leader>/", ":Grep ", { silent = false, desc = "Fuzzy grep" })
     vim.keymap.set("n", "<leader>?", ":Files! --type f ", { silent = false, desc = "Fuzzy grep files" })
     vim.keymap.set("n", "<leader>fb", ":Buffers! ", { silent = false, desc = "Fuzzy buffer list" })
-    vim.keymap.set("n", "<leader>fw", function() _fuzzy_grep(vim.fn.expand("<cword>"), false) end,
-        { silent = false, desc = "Fuzzy grep current word" })
-    vim.keymap.set("n", "<leader>fW", function() _fuzzy_grep(vim.fn.expand("<cWORD>"), true) end,
-        { silent = false, desc = "Fuzzy grep current WORD" })
-    vim.keymap.set("n", "<leader>fl", "<CMD>FuzzyList<CR>", { silent = false, desc = "Fuzzy list" })
+
+    vim.keymap.set('n', '<leader>fw', function()
+        local word = vim.fn.expand('<cword>')
+        if word ~= '' then fuzzy.grep({ word }) end
+    end, { desc = 'Grep word' })
+
+    vim.keymap.set('n', '<leader>fW', function()
+        local word = vim.fn.expand('<cWORD>')
+        if word ~= '' then fuzzy.grep({ '-F', word }) end
+    end, { desc = 'Grep WORD (literal)' })
 end
 -- } fuzzy
 
