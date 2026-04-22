8fcdb29c7f2da94e257e761296537a287a8ed7b4
Anoop K. Chandran <anoopkcn@gmail.com>
2026-04-22 17:36:49 +0200

    Remove fzf and simplify habamax

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,8 @@
 -- Neovim Configuration (init.lua)
 -- LICENSE: MIT
 -- AUTHOR:  @anoopkcn
-
+require("vim._core.ui2").enable({})
 vim.g.mapleader = " "
-vim.opt.laststatus = 0
 vim.g.netrw_liststyle = 1
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -28,12 +27,30 @@ vim.opt.pumborder = "rounded"
 vim.opt.splitkeep = "screen"
 vim.opt.splitbelow = true
 vim.opt.switchbuf:append("useopen")
-vim.cmd("colorscheme habamax")
 vim.opt.title = true
 vim.opt.titlestring = "%t%m%r"
 vim.opt.termguicolors = true
 
--- keymaps {
+vim.cmd([[colorscheme habamax]])
+local text = "#bcbcbc"
+local text_faint = "#767676"
+local cmdline_bg = "#303030"
+vim.api.nvim_set_hl(0, "NormalFloat", { fg = text })
+vim.api.nvim_set_hl(0, "Pmenu", { fg = text, bg = "NONE" })
+vim.api.nvim_set_hl(0, "PmenuSel", { fg = text, bg = cmdline_bg })
+vim.api.nvim_set_hl(0, "PmenuBorder", { fg = cmdline_bg, bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = cmdline_bg, bg = "NONE" })
+vim.api.nvim_set_hl(0, "StatusLine", { fg = text, bg = cmdline_bg })
+vim.api.nvim_set_hl(0, "StatusLineNC", { fg = text_faint, bg = cmdline_bg })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = cmdline_bg, bg = "NONE" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>",
     { noremap = true, silent = true, desc = "Disable Space" })
 vim.keymap.set({ "n", "v" }, "<C-Space>", "<Nop>",
@@ -76,13 +93,13 @@ vim.keymap.set("n", "<leader>fe", "<cmd>Ex<cr>", { desc = "Open parent directory
 vim.keymap.set("n", "<leader>z", "<cmd>terminal<cr>", { desc = "Open terminal" })
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
 
-
 ToggleQuickfixList = function()
     local qf = vim.fn.getqflist({ winid = 1 }).winid
     if qf > 0 then vim.cmd("cclose") else vim.cmd("copen") end
 end
 vim.keymap.set("n", "<leader>fq", ToggleQuickfixList,
     { noremap = true, silent = true, desc = "Toggle quickfixlist" })
+
 TimestampInsert = function()
     local raw_timestamp = os.date("%FT%T")
     local timestamp_str = string.format("%s", raw_timestamp or "")
@@ -90,14 +107,12 @@ TimestampInsert = function()
 end
 vim.keymap.set("n", "<leader>'", TimestampInsert,
     { noremap = true, silent = true, desc = "Insert current timestamp" })
--- } keymaps
 
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
     callback = function() vim.highlight.on_yank() end,
 })
 
--- LSP settings {
 vim.lsp.enable({ "clangd", "lua_ls", "pyright", "marksman", })
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("CustomLspAttach", { clear = true }),
@@ -111,46 +126,187 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, map_opts)
     end,
 })
--- } LSP settings
 
-require("plugins")
+vim.pack.add({
+    {
+        src = "https://github.com/lewis6991/gitsigns.nvim",
+        version = "main"
+    },
+    {
+        src = "https://github.com/tpope/vim-surround",
+        name = "vim-surround"
+    },
+    {
+        src = "https://github.com/tpope/vim-unimpaired",
+        name = "vim-unimpaired",
+        version = "master"
+    },
+    {
+        src = "https://github.com/github/copilot.vim",
+        name = "copilot"
+    },
+})
+-- } packages
 
-
--- Theme settings {
-local text = "#bcbcbc"
-local text_faint = "#767676"
-local cmdline_bg = "#303030"
-vim.api.nvim_set_hl(0, "NormalFloat", { fg = text })
-vim.api.nvim_set_hl(0, "Pmenu", { fg = text, bg = "NONE" })
-vim.api.nvim_set_hl(0, "PmenuSel", { fg = text, bg = cmdline_bg })
-vim.api.nvim_set_hl(0, "PmenuBorder", { fg = cmdline_bg, bg = "NONE" })
-vim.api.nvim_set_hl(0, "FloatBorder", { fg = cmdline_bg, bg = "NONE" })
-vim.api.nvim_set_hl(0, "StatusLine", { fg = cmdline_bg, bg = cmdline_bg })
-vim.api.nvim_set_hl(0, "StatusLineNC", { fg = cmdline_bg, bg = cmdline_bg })
-vim.api.nvim_set_hl(0, "WinSeparator", { fg = cmdline_bg, bg = "NONE" })
-vim.api.nvim_set_hl(0, "WinBar", { fg = text, bg = "NONE" })
-vim.api.nvim_set_hl(0, "WinBarNC", { fg = text_faint, bg = "NONE" })
-
-local function set_habamax_tabline()
-    vim.api.nvim_set_hl(0, "TabLine", { fg = "#9e9e9e", bg = "#262626" })
-    vim.api.nvim_set_hl(0, "TabLineFill", { fg = "#767676", bg = "#262626" })
-    vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#e4e4e4", bg = "#1a3456", bold = true })
+-- csub {
+vim.pack.add({
+    {
+        src = "https://github.com/anoopkcn/csub.nvim",
+        name = "csub"
+    },
+})
+local ok, csub = pcall(require, "csub")
+if ok then
+    csub.setup({
+        default_mode = nil,
+        handlers = {
+            { match = "FuzzyGrep",    mode = "replace" },
+            { match = "vimgrep",      mode = "replace" },
+            { match = "FuzzyBuffers", mode = "buffers" },
+            { match = "FuzzyFiles",   mode = nil }, -- disabled
+            { match = "make",         mode = nil }, -- disabled
+        },
+    })
+    vim.keymap.set("n", "<leader>s", "<cmd>Csub<cr>",
+        { desc = "Open Csub buffer for current active quickfix list" })
 end
+-- } csub
 
-set_habamax_tabline()
-vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "habamax",
-    callback = set_habamax_tabline,
+-- fuzzy {
+vim.pack.add({
+    {
+        src = "https://github.com/anoopkcn/fuzzy.nvim",
+        name = "fuzzy"
+    },
+})
+local ok_fuzzy, fuzzy = pcall(require, "fuzzy")
+if ok_fuzzy then
+    fuzzy.setup()
+
+    -- vim.keymap.set('n', ']q', '<CMD>FuzzyNext<CR>')
+    -- vim.keymap.set('n', '[q', '<CMD>FuzzyPrev<CR>')
+    vim.keymap.set("n", "<leader>fl", "<CMD>FuzzyList<CR>", { silent = false, desc = "Fuzzy list" })
+    vim.keymap.set("n", "<leader>/", ":Grep ", { silent = false, desc = "Fuzzy grep" })
+    vim.keymap.set("n", "<leader>?", ":Files! --type f ", { silent = false, desc = "Fuzzy grep files" })
+    vim.keymap.set("n", "<leader>ff", ":Files ", { silent = false, desc = "Fuzzy grep files" })
+    vim.keymap.set("n", "<leader>fb", ":Buffers! ", { silent = false, desc = "Fuzzy buffer list" })
+
+    vim.keymap.set('n', '<leader>fw', function()
+        local word = vim.fn.expand('<cword>')
+        if word ~= '' then fuzzy.grep({ word }) end
+    end, { desc = 'Grep word' })
+
+    vim.keymap.set('n', '<leader>fW', function()
+        local word = vim.fn.expand('<cWORD>')
+        if word ~= '' then fuzzy.grep({ '-F', word }) end
+    end, { desc = 'Grep WORD (literal)' })
+end
+-- } fuzzy
+
+-- filemarks {
+vim.pack.add({
+    {
+        src = "https://github.com/anoopkcn/filemarks.nvim",
+        name = "filemarks"
+    },
+})
+local ok_filemarks, filemarks = pcall(require, "filemarks")
+if ok_filemarks then
+    filemarks.setup({
+        dir_open_cmd = "Explore"
+        -- dir_open_cmd = "Oil %s"
+    })
+    vim.keymap.set("n", "<leader>l", "<cmd>bot FilemarksList<cr>",
+        { noremap = true, silent = true, desc = "show file marks list" })
+end
+-- } filemarks
+
+-- fugitive {
+vim.pack.add({
+    {
+        src = "https://github.com/tpope/vim-fugitive",
+        name = "vim-fugitive"
+    },
+})
+vim.keymap.set("n", "<leader>G", "<CMD>rightbelow vertical Git<CR>",
+    { noremap = true, silent = true, desc = "Open Git interface" })
+vim.keymap.set("n", "<leader>gl", "<CMD>rightbelow vertical Git log<CR>",
+    { noremap = true, silent = true, desc = "Git log" })
+-- } fugitive
+
+-- treesitter {
+vim.pack.add({
+    {
+        src = "https://github.com/nvim-treesitter/nvim-treesitter",
+        name = "treesitter",
+        branch = "main",
+    },
 })
 
--- make background transparent
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
--- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
+  local name, kind = ev.data.spec.name, ev.data.kind
+  if name == 'nvim-treesitter' and kind == 'update' then
+    if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
+    vim.cmd('TSUpdate')
+  end
+end })
 
--- } Theme settings
+local setup_treesitter = function()
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup({})
+    local ensure_installed = {
+        "vim",
+        "vimdoc",
+        "rust",
+        "c",
+        "cpp",
+        "go",
+        "html",
+        "css",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "typescript",
+        "bash",
+        "lua",
+        "python",
+    }
+
+    local config = require("nvim-treesitter.config")
+
+    local already_installed = config.get_installed()
+    local parsers_to_install = {}
+
+    for _, parser in ipairs(ensure_installed) do
+        if not vim.tbl_contains(already_installed, parser) then
+            table.insert(parsers_to_install, parser)
+        end
+    end
+
+    if #parsers_to_install > 0 then
+        treesitter.install(parsers_to_install)
+    end
+
+    local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        callback = function(args)
+            if vim.list_contains(treesitter.get_installed(), vim.treesitter.language.get_lang(args.match)) then
+                vim.treesitter.start(args.buf)
+            end
+        end,
+    })
+end
+
+setup_treesitter()
+-- } treesitter
+
+-- vim.pack.add({
+-- {
+--     src = "https://github.com/mfussenegger/nvim-dap",
+--     name = "nvim-dap",
+--     version = "master"
+-- },
+-- })
+-- require("dap-config")
