8d158d47f8fde91bf6fe68368df0a68b696e901a
Anoop K. Chandran <anoopkcn@gmail.com>
2026-06-06 08:34:50 +0200

    Improve kity sessions

--- zsh/fzf.sh
+++ zsh/fzf.sh
@@ -3,18 +3,15 @@
 # (e.g. kitty overlay scripts), so the theme is consistent everywhere.
 
 export FZF_DEFAULT_OPTS='--layout=reverse
---color=fg:#e0e2e9,bg:-1,hl:#f7e19e
---color=fg+:#e0e2e9,bg+:#292b33,hl+:#f7e19e
---color=gutter:#1d1f27,border:#505257,separator:#505257
---color=info:#b26cc7,prompt:#b1dafb,pointer:#f5c3bb
---color=marker:#c1f4c4,spinner:#b26cc7,header:#a6f5f6
---color=preview-fg:#e0e2e9,preview-bg:-1,preview-border:#505257
---color=query:#e0e2e9,disabled:#505257,label:#b2b5b9
+--info=right 
+--border=rounded
 --bind=alt-p:toggle-preview
 --bind=ctrl-u:preview-half-page-up
 --bind=ctrl-d:preview-half-page-down
 --bind=ctrl-y:preview-up
---bind=ctrl-e:preview-down'
+--bind=ctrl-e:preview-down
+--color=fg:-1,bg:-1,bg+:#32373f,pointer:#8ED6FE,hl:#dc6a74,hl+:#dc6a74,gutter:#292d33
+'
 
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always --style=numbers --line-range=:200 {}' --preview-window=hidden"
 export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200' --preview-window=hidden"
