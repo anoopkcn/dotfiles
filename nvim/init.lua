8bef18303371fc349d1a0a15feabd6a4c41e874c
Anoop K. Chandran <anoopkcn@gmail.com>
2026-06-10 01:20:26 +0200

    Add Oil settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -234,7 +234,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 
 vim.opt.runtimepath:prepend('/Users/akc/develop/stitch.nvim')
+require("stitch").setup({
+    context = 0
+})
 vim.opt.runtimepath:prepend('/Users/akc/develop/oil.nvim')
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+    local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+    local dir = require("oil").get_current_dir(bufnr)
+    if dir then
+        return dir -- full absolute path
+    else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+    end
+end
+
 require("oil").setup({
     columns = {
         -- "icon",
@@ -246,7 +261,10 @@ require("oil").setup({
     skip_confirmation = true,
     view_options = {
         show_hidden = true,
-    }
+    },
+    win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+    },
 })
 
 map("n", "<leader>fe", "<CMD>Oil<CR>", { silent = true, desc = "Open file explorer" })
